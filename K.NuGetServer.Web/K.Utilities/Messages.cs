66b6a4c67e2d56453c829aeb0d53d254d96b1dd7
Kuro <kuro_q@live.com>
2017-04-03 22:41:49 +0800

    -> add utlities & test projects -> add moq references -> add stylecop.analyzers references

---
+++ K.NuGetServer.Web/K.Utilities/Messages.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="Messages.cs" company="K&amp;T">
+// Copyright (c) K&amp;T. All rights reserved.
+// </copyright>
+
+namespace K.Utilities
+{
+    /// <summary>
+    /// Messages
+    /// </summary>
+    public class Messages
+    {
+        /// <summary>
+        /// messages
+        /// </summary>
+        /// <returns>sssss</returns>
+        public virtual string GetTestMessage()
+        {
+            return $"this is a test message from ${typeof(Messages).FullName}";
+        }
+    }
+}
