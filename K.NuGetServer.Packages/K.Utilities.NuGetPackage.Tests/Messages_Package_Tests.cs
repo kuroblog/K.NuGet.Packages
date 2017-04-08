7f350c51f4a0b4bb32db4fccef92f391f950c103
Kuro <kuro_q@live.com>
2017-04-08 22:27:22 +0800

    -> clean project files

---
+++ K.NuGetServer.Packages/K.Utilities.NuGetPackage.Tests/Messages_Package_Tests.cs
@@ -0,0 +1,29 @@
+﻿
+namespace K.Utilities.NuGetPackage.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class Messages_Package_Tests
+    {
+        //private Mock<Messages> mockMessages;
+
+        //[TestInitialize]
+        //public void Initialize()
+        //{
+        //    mockMessages = new Mock<Messages>();
+        //}
+
+        //[TestMethod]
+        //public void ToString_Test()
+        //{
+        //    var expected = nameof(ToString_Test);
+        //    mockMessages.Setup(m => m.GetTestMessage()).Returns(expected);
+
+        //    var actual = mockMessages.Object.GetTestMessage();
+
+        //    Assert.AreEqual(expected, actual);
+        //}
+    }
+}
---
+++ K.NuGetServer.Packages/K.Utilities/Messages/Messages.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="Messages.cs" company="K&amp;T">
+// Copyright (c) K&amp;T. All rights reserved.
+// </copyright>
+
+namespace K.Utilities.Messages
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Provide message processing
+    /// </summary>
+    public class Messages
+    {
+        /// <summary>
+        /// Get a test message
+        /// </summary>
+        /// <returns>
+        /// return a test message, type is
+        /// <see cref="string"/>
+        /// </returns>
+        [ExcludeFromCodeCoverage]
+        public virtual string GetTestMessage()
+        {
+            return $"this is a test message from ${typeof(Messages).FullName}";
+        }
+    }
+}
